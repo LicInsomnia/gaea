b46f84bf40b1f6d60296508c1d1ae3af8ad24b94
Insomnia <insomnia@tincery.com>
2020-09-29 11:16:30 +0800

    Reorganization

--- gaea-core/src/main/java/com/tincery/gaea/GaeaCoreAutoConfiguration.java
+++ gaea-core/src/main/java/com/tincery/gaea/GaeaCoreAutoConfiguration.java
@@ -11,20 +11,20 @@ import org.springframework.context.annotation.Configuration;
  * @author gxz gongxuanzhang@foxmail.com
  **/
 @Configuration
-@ComponentScans(
-        {@ComponentScan("com.tincery.gaea.core.*")
+@ComponentScans (
+        {@ComponentScan ("com.tincery.gaea.core.*")
         })
 public class GaeaCoreAutoConfiguration {
 
 
-        @Bean
-        public EnvInit getEnvInit(){
-                return new EnvInit();
-        }
+    @Bean
+    public EnvInit getEnvInit() {
+        return new EnvInit();
+    }
 
-        @Bean
-        public CommonConfigInit getCommonConfigInit(){
-                return new CommonConfigInit();
-        }
+    @Bean
+    public CommonConfigInit getCommonConfigInit() {
+        return new CommonConfigInit();
+    }
 
 }
