69fe23107970283b289b7b702f10ee137104c21b
gxz <514190950@qq.com>
2020-10-27 17:36:21 +0800

    更新资产相关内容

--- gaea-core/src/main/java/com/tincery/gaea/core/base/component/support/AssetDetector.java
+++ gaea-core/src/main/java/com/tincery/gaea/core/base/component/support/AssetDetector.java
@@ -1,5 +1,6 @@
 package com.tincery.gaea.core.base.component.support;
 
+import com.tincery.gaea.api.base.IpRange;
 import com.tincery.gaea.api.dm.AssetConfigDO;
 import com.tincery.gaea.core.base.dao.AssetConfigDao;
 import com.tincery.gaea.core.base.tool.ToolUtils;
@@ -8,6 +9,7 @@ import javafx.util.Pair;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import org.springframework.util.CollectionUtils;
 
 import java.util.HashMap;
 import java.util.List;
@@ -25,34 +27,48 @@ public class AssetDetector implements InitializationRequired {
     @Autowired
     private AssetConfigDao assetConfigDao;
     /**
-     * assetFlg = 0:不是资产
-     * assetFlg = 1:客户端资产
-     * assetFlg = 2:服务端资产
-     * assetFlg = 3:都是资产
+     * assetFlag = 0:不是资产
+     * assetFlag = 1:客户端资产
+     * assetFlag = 2:服务端资产
+     * assetFlag = 3:都是资产
      */
-    private final Map<String, AssetConfigDO> uniqueAssetMap = new HashMap<>();
+    private final Map<Long, AssetConfigDO> uniqueAssetMap = new HashMap<>();
 
     private final Map<Pair<Long, Long>, AssetConfigDO> rangeAssetMap = new HashMap<>();
 
+    /****
+     * 匹配IP是否是资产
+     * @author gxz
+     * @param ip  IP
+     * @return boolean 是否是资产
+     **/
     public boolean checkAssetIp(String ip) {
+        long ipN = ToolUtils.IP2long(ip);
+        return checkAssetIp(ipN);
+    }
+
+    public boolean checkAssetIp(long ip) {
         if (this.uniqueAssetMap.containsKey(ip)) {
             return true;
         }
-        long ipN = ToolUtils.IP2long(ip);
         for (Pair<Long, Long> pair : this.rangeAssetMap.keySet()) {
             Long minIp = pair.getKey();
             Long maxIp = pair.getValue();
-            if (minIp <= ipN && ipN <= maxIp) {
+            if (minIp <= ip && ip <= maxIp) {
                 return true;
             }
         }
         return false;
     }
 
+
     /**
-     * assetFlag = 0:不是资产 assetFlag = 1:客户端资产 assetFlag = 2:服务端资产 assetFlag = 3:都是资产
+     * assetFlag = 0:不是资产
+     * assetFlag = 1:客户端资产
+     * assetFlag = 2:服务端资产
+     * assetFlag = 3:都是资产
      */
-    public int checkSessionAsset(String clientIp, String serverIp) {
+    public int getAssetFlag(String clientIp, String serverIp) {
         int assetFlag = 0;
         assetFlag += checkAssetIp(clientIp) ? 1 : 0;
         assetFlag += checkAssetIp(serverIp) ? 2 : 0;
@@ -60,10 +76,14 @@ public class AssetDetector implements InitializationRequired {
     }
 
     public AssetConfigDO getAsset(String ip) {
-        if (this.uniqueAssetMap.containsKey(ip)) {
-            return this.uniqueAssetMap.get(ip);
-        }
         Long ipN = ToolUtils.IP2long(ip);
+        return getAsset(ipN);
+    }
+
+    public AssetConfigDO getAsset(long ipN) {
+        if (this.uniqueAssetMap.containsKey(ipN)) {
+            return this.uniqueAssetMap.get(ipN);
+        }
         for (Pair<Long, Long> pair : this.rangeAssetMap.keySet()) {
             if (pair.getKey() <= ipN && ipN <= pair.getValue()) {
                 return this.rangeAssetMap.get(pair);
@@ -77,17 +97,20 @@ public class AssetDetector implements InitializationRequired {
     public void init() {
         List<AssetConfigDO> activityData = assetConfigDao.getActivityData();
         activityData.forEach(this::putAsset);
-        log.info("成功加载{}条独立IP资产规则，{}条范围资产规则",this.uniqueAssetMap.size(),this.rangeAssetMap.size());
-    }
-    private void putAsset(AssetConfigDO asset){
-/*
-        if (asset.isUnique()) {
-            this.uniqueAssetMap.put(asset.getIp(), asset);
-        } else {
-            this.rangeAssetMap.put(new Pair<>(asset.getMinIp_n(), asset.getMaxIp_n()), asset);
-        }
-*/
+        log.info("成功加载{}条独立IP资产规则，{}条范围资产规则", this.uniqueAssetMap.size(), this.rangeAssetMap.size());
     }
 
+    private void putAsset(AssetConfigDO assetConfig) {
+        List<Long> ips = assetConfig.getIps();
+        List<IpRange> ipRanges = assetConfig.getIpRanges();
+        if (!CollectionUtils.isEmpty(ips)) {
+            ips.forEach(ip -> this.uniqueAssetMap.put(ip, assetConfig));
+        }
+
+        if (!CollectionUtils.isEmpty(ipRanges)) {
+            ipRanges.forEach(ipRange -> this.rangeAssetMap.put(new Pair<>(ipRange.getMinIp(), ipRange.getMaxIp()),
+                    assetConfig));
+        }
+    }
 
 }
