f37422a57dbaa0bd796987a08036941526bdc3da
gxz <514190950@qq.com>
2020-10-29 14:38:16 +0800

    资产匹配相关内容

--- gaea-core/src/main/java/com/tincery/gaea/api/dm/AssetConfigs.java
+++ gaea-core/src/main/java/com/tincery/gaea/api/dm/AssetConfigs.java
@@ -2,13 +2,15 @@ package com.tincery.gaea.api.dm;
 
 import com.alibaba.fastjson.JSONObject;
 import com.tincery.gaea.api.base.AlarmMaterialData;
+import com.tincery.gaea.api.base.ThPredicate;
 import com.tincery.gaea.core.base.component.support.AssetDetector;
 import com.tincery.gaea.core.base.mgt.HeadConst;
+import org.springframework.util.CollectionUtils;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 import java.util.function.BiFunction;
-import java.util.function.BiPredicate;
 import java.util.function.Function;
 
 /**
@@ -16,6 +18,18 @@ import java.util.function.Function;
  * assetconfig的工具类
  **/
 public class AssetConfigs {
+
+    private static BiFunction<JSONObject, AssetConfigDO, AlarmMaterialData> createAlarm;
+
+    private static Function<JSONObject, Integer> getProtocol;
+
+    private static Function<JSONObject, Integer> getPort;
+
+    static {
+        createAlarm = (jsonObject, assetConfigDO) -> new AlarmMaterialData();
+        getProtocol = jsonObject -> jsonObject.getIntValue(HeadConst.CSV.PROTOCOL);
+        getPort = jsonObject -> jsonObject.getIntValue(HeadConst.CSV.SERVER_PORT);
+    }
 
     /****
      * 作为客户端是一条资产的时候调用此方法  此方法将返回方法所检测的所有告警素材
@@ -25,38 +39,71 @@ public class AssetConfigs {
      * @return 产生的报警报告内容
      **/
     public static List<AlarmMaterialData> detectorClient(JSONObject assetJson, AssetDetector assetDetector) {
-        List<AlarmMaterialData> result = new ArrayList<>();
         AssetConfigDO assetConfig = assetDetector.getAsset(assetJson.getLong(HeadConst.CSV.CLIENT_IP_N));
-        AssetConfigDO.OutInputFilter out = assetConfig.getBlackList().getOut();
-        if (true) {
-            // 命中之后继续匹配
-
-
-        } else {
-            // 命中之后不继续匹配
+        // 先判断黑名单 命中告警
+        if (check(assetJson, assetConfig, ListType.BLACK, OutInput.OUT, Border.DOMESTIC)) {
+            // 境内
+            return Collections.singletonList(createAlarm.apply(assetJson, assetConfig));
         }
-
+        if(check(assetJson, assetConfig, ListType.BLACK, OutInput.OUT, Border.OVERSEAS)){
+            // 境外
+            return Collections.singletonList(createAlarm.apply(assetJson, assetConfig));
+        }
+        // 判断白名单 如果命中则直接返回
+        if(check(assetJson, assetConfig, ListType.WHITE, OutInput.OUT, Border.DOMESTIC)){
+            return null;
+        }
+        if(check(assetJson, assetConfig, ListType.WHITE, OutInput.OUT, Border.OVERSEAS)){
+            return null;
+        }
+        // 白名单没返回 则告警
+        return Collections.singletonList(createAlarm.apply(assetJson, assetConfig));
     }
 
     public static List<AlarmMaterialData> detectorServer(JSONObject assetJson, AssetDetector assetDetector) {
-        return assetJson;
+        AssetConfigDO assetConfig = assetDetector.getAsset(assetJson.getLong(HeadConst.CSV.SERVER_IP_N));
+        // 先判断黑名单 命中告警
+        if (check(assetJson, assetConfig, ListType.BLACK, OutInput.IN, Border.DOMESTIC)) {
+            // 境内
+            return Collections.singletonList(createAlarm.apply(assetJson, assetConfig));
+        }
+        if(check(assetJson, assetConfig, ListType.BLACK, OutInput.IN, Border.OVERSEAS)){
+            // 境外
+            return Collections.singletonList(createAlarm.apply(assetJson, assetConfig));
+        }
+        // 判断白名单 如果命中则直接返回
+        if(check(assetJson, assetConfig, ListType.WHITE, OutInput.IN, Border.DOMESTIC)){
+            return null;
+        }
+        if(check(assetJson, assetConfig, ListType.WHITE, OutInput.IN, Border.OVERSEAS)){
+            return null;
+        }
+        // 白名单没返回 则告警
+        return Collections.singletonList(createAlarm.apply(assetJson, assetConfig));
     }
 
     public static List<AlarmMaterialData> detectorClientAndServer(JSONObject assetJson, AssetDetector assetDetector) {
-        return assetJson;
-    }
-
-
-    private static boolean checkListAndStop(JSONObject assetJson, AssetConfigDO.OutInputFilter filter,
-                                            FilterType type) {
-
-        filter.getDomestic().stream().anyMatch(domesticFilter -> {
-            if (domesticFilter.isUnique()) {
-                return assetJsonOUT_DOMESTIC
-            }
-        })
+        List<AlarmMaterialData> result = new ArrayList<>();
+        List<AlarmMaterialData> clientAlarm = detectorClient(assetJson, assetDetector);
+        if(!CollectionUtils.isEmpty(clientAlarm)){
+            result.addAll(clientAlarm);
+        }
+        List<AlarmMaterialData> serverAlarm = detectorServer(assetJson, assetDetector);
+        if(!CollectionUtils.isEmpty(serverAlarm)){
+            result.addAll(serverAlarm);
+        }
+        return result;
 
     }
+
+    public static boolean check(JSONObject assetJson, AssetConfigDO assetConfig,
+                                ListType listType, OutInput outInput, Border border) {
+        AssetConfigDO.BlackOrWhiteList blackOrWhiteList = listType.function.apply(assetConfig);
+        AssetConfigDO.OutInputFilter outInputFilter = outInput.getOutInputFunction.apply(blackOrWhiteList);
+        return border.getHitableFunction.test(assetJson,outInputFilter,outInput.getIpFunction);
+
+    }
+
 
 
     public enum ListType {
@@ -68,107 +115,50 @@ public class AssetConfigs {
         ListType(Function<AssetConfigDO, AssetConfigDO.BlackOrWhiteList> function) {
             this.function = function;
         }
-
-        public AssetConfigDO.BlackOrWhiteList getList(AssetConfigDO assetConfig) {
-            return this.function.apply(assetConfig);
-        }
     }
 
     public enum OutInput {
-        OUT(AssetConfigDO.BlackOrWhiteList::getOut),
-        IN(AssetConfigDO.BlackOrWhiteList::getIn);
+        OUT(AssetConfigDO.BlackOrWhiteList::getOut, (asset) -> asset.getLong(HeadConst.CSV.SERVER_IP_N)),
+        IN(AssetConfigDO.BlackOrWhiteList::getIn, (asset) -> asset.getLong(HeadConst.CSV.CLIENT_IP_N));
 
-        private Function<AssetConfigDO.BlackOrWhiteList, AssetConfigDO.OutInputFilter> function;
+        private Function<AssetConfigDO.BlackOrWhiteList, AssetConfigDO.OutInputFilter> getOutInputFunction;
+
+        private Function<JSONObject, Long> getIpFunction;
 
 
-        OutInput(Function<AssetConfigDO.BlackOrWhiteList, AssetConfigDO.OutInputFilter> function) {
-            this.function = function;
+        OutInput(Function<AssetConfigDO.BlackOrWhiteList, AssetConfigDO.OutInputFilter> getOutInputFunction,
+                 Function<JSONObject
+                         , Long> getIpFunction) {
+            this.getOutInputFunction = getOutInputFunction;
+            this.getIpFunction = getIpFunction;
         }
 
-
-
-        public AssetConfigDO.OutInputFilter getFilter(AssetConfigDO.BlackOrWhiteList blackOrWhiteList) {
-            return this.function.apply(blackOrWhiteList);
-        }
     }
 
     public enum Border {
-        DOMESTIC(AssetConfigs::outDomestic),
-        OVERSEAS(AssetConfigs::inDomestic);
+        DOMESTIC(AssetConfigs::domesticHit),
+        OVERSEAS(AssetConfigs::overseasHit);
 
-        // 判断是否命中
-        private BiPredicate<JSONObject, AssetConfigDO.OutInputFilter> biPredicate;
+        public ThPredicate<JSONObject, AssetConfigDO.OutInputFilter, Function<JSONObject, Long>> getHitableFunction;
 
-        // 告警素材是如何由规则和资产产生的
-        private BiFunction<JSONObject, AssetConfigDO, AlarmMaterialData> createAlarmFunction;
-
-        Border(BiPredicate<JSONObject, AssetConfigDO.OutInputFilter> biPredicate,
-               BiFunction<JSONObject, AssetConfigDO, AlarmMaterialData> createAlarmFunction) {
-            this.biPredicate = biPredicate;
-            this.createAlarmFunction = createAlarmFunction;
-        }
-
-        public boolean
-    }
-
-    public enum FilterType {
-        OUT_DOMESTIC(AssetConfigs::outDomestic),
-        OUT_OVERSEAS(AssetConfigs::outOverseas),
-        IN_DOMESTIC(AssetConfigs::inDomestic),
-        IN_OVERSEAS(AssetConfigs::inOverseas);
-
-        // 判断是否命中
-        private BiPredicate<JSONObject, AssetConfigDO.OutInputFilter> biPredicate;
-
-        // 告警素材是如何由规则和资产产生的
-        private BiFunction<JSONObject, AssetConfigDO, AlarmMaterialData> createAlarmFunction;
-
-        FilterType(BiPredicate<JSONObject, AssetConfigDO.OutInputFilter> biPredicate,
-                   BiFunction<JSONObject, AssetConfigDO, AlarmMaterialData> createAlarmFunction) {
-            this.biPredicate = biPredicate;
-            this.createAlarmFunction = createAlarmFunction;
+        Border(ThPredicate<JSONObject, AssetConfigDO.OutInputFilter, Function<JSONObject, Long>> getHitableFunction) {
+            this.getHitableFunction = getHitableFunction;
         }
     }
 
-    public AlarmMaterialData hitAndStop(JSONObject assetJson, AssetConfigDO.OutInputFilter outInputFilter,
-                                        FilterType mode) {
-        if (mode.biPredicate.test(assetJson, outInputFilter)) {
-            return mode.createAlarmFunction.apply(assetJson, outInputFilter)
-        }
+    private static boolean overseasHit(JSONObject assetJson, AssetConfigDO.OutInputFilter outInputFilter,
+                                       Function<JSONObject, Long> getIp) {
+        return outInputFilter.getOverseas().hit(getIp.apply(assetJson), getProtocol.apply(assetJson),
+                getPort.apply(assetJson));
     }
 
-    private static boolean outDomestic(JSONObject jsonObject, AssetConfigDO.OutInputFilter outInputFilter) {
-        return outInputFilter.getDomestic().stream().anyMatch(domesticFilter -> {
-            long ip = jsonObject.getLong(HeadConst.CSV.SERVER_IP_N);
-            int protocol = jsonObject.getInteger(HeadConst.CSV.PROTOCOL);
-            int port = jsonObject.getInteger(HeadConst.CSV.SERVER_PORT);
-            return domesticFilter.hit(ip, protocol, port);
-        });
-    }
+    private static boolean domesticHit(JSONObject assetJson, AssetConfigDO.OutInputFilter outInputFilter,
+                                       Function<JSONObject, Long> getIp) {
+        return outInputFilter.getDomestic()
+                .stream().anyMatch(domesticFilter ->
+                        domesticFilter.hit(getIp.apply(assetJson), getProtocol.apply(assetJson),
+                                getPort.apply(assetJson)));
 
-    private static boolean outOverseas(JSONObject jsonObject, AssetConfigDO.OutInputFilter outInputFilter) {
-        AssetConfigDO.OverseasFilter overseas = outInputFilter.getOverseas();
-        long ip = jsonObject.getLong(HeadConst.CSV.SERVER_IP_N);
-        int protocol = jsonObject.getInteger(HeadConst.CSV.PROTOCOL);
-        int port = jsonObject.getInteger(HeadConst.CSV.SERVER_PORT);
-        return overseas.hit(ip, protocol, port);
-    }
-
-    private static boolean inDomestic(JSONObject jsonObject, AssetConfigDO.OutInputFilter outInputFilter) {
-        return outInputFilter.getDomestic().stream().anyMatch(domesticFilter -> {
-            long ip = jsonObject.getLong(HeadConst.CSV.CLIENT_IP_N);
-            int protocol = jsonObject.getInteger(HeadConst.CSV.PROTOCOL);
-            int port = jsonObject.getInteger(HeadConst.CSV.CLIENT_PORT);
-            return domesticFilter.hit(ip, protocol, port);
-        });
-    }
-
-    private static boolean inOverseas(JSONObject jsonObject, AssetConfigDO.OutInputFilter outInputFilter) {
-        AssetConfigDO.OverseasFilter overseas = outInputFilter.getOverseas();
-        long ip = jsonObject.getLong(HeadConst.CSV.CLIENT_IP_N);
-        int protocol = jsonObject.getInteger(HeadConst.CSV.PROTOCOL);
-        int port = jsonObject.getInteger(HeadConst.CSV.CLIENT_PORT);
-        return overseas.hit(ip, protocol, port);
     }
 
 
--- gaea-dm-asset/src/main/java/com/tincery/gaea/datamarket/asset/execute/AssetReceiver.java
+++ gaea-dm-asset/src/main/java/com/tincery/gaea/datamarket/asset/execute/AssetReceiver.java
@@ -3,32 +3,29 @@ package com.tincery.gaea.datamarket.asset.execute;
 import com.alibaba.fastjson.JSON;
 import com.alibaba.fastjson.JSONObject;
 import com.tincery.gaea.api.base.AlarmMaterialData;
-import com.tincery.gaea.api.dm.AssetConfigDO;
 import com.tincery.gaea.api.dm.AssetConfigs;
-import com.tincery.gaea.api.dm.AssetDataDTO;
-import com.tincery.gaea.api.dm.AssetCondition;
 import com.tincery.gaea.core.base.component.Receiver;
 import com.tincery.gaea.core.base.component.support.AssetDetector;
-import com.tincery.gaea.core.base.dao.AssetConditionDao;
-import jdk.nashorn.internal.ir.EmptyNode;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.util.CollectionUtils;
 
 import javax.jms.JMSException;
 import javax.jms.TextMessage;
-import java.io.*;
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
-import java.util.Comparator;
 import java.util.List;
-import java.util.Map;
 import java.util.Optional;
+import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.function.BiFunction;
-import java.util.function.Function;
-import java.util.stream.Collectors;
 
 /**
  * @author gxz
@@ -40,21 +37,55 @@ import java.util.stream.Collectors;
 public class AssetReceiver implements Receiver {
 
 
+    private List<AlarmMaterialData> alarmList = new CopyOnWriteArrayList<>();
+
+    private static final int ALARM_WRITE_COUNT = 10000;
+
     @Autowired
     private AssetDetector assetDetector;
 
     @Override
     public void receive(TextMessage textMessage) throws JMSException {
         File file = new File(textMessage.getText());
-        List<JSONObject> allJsonObject = new ArrayList<>();
+        List<JSONObject> allAssetJson = new ArrayList<>();
         try (BufferedReader bufferedReader = new BufferedReader(new FileReader(file))) {
             String line;
             while ((line = bufferedReader.readLine()) != null) {
                 JSONObject assetJson = JSON.parseObject(line);
                 int assetFlag = assetJson.getIntValue("assetFlag");
-                JSONObject jsonObject = AssetFlag.jsonRun(assetFlag, assetJson);
-
+                List<AlarmMaterialData> alarmMaterialDataList = AssetFlag.jsonRun(assetFlag, assetJson, assetDetector);
+                if (!CollectionUtils.isEmpty(alarmMaterialDataList)) {
+                    alarmAdd(alarmMaterialDataList);
+                }
+                allAssetJson.add(assetJson);
             }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        free(allAssetJson);
+    }
+
+
+    private void free(List<JSONObject> allAssetJson) {
+        // 这里还得分类
+
+
+        writeAlarm();
+    }
+
+    private synchronized void alarmAdd(List<AlarmMaterialData> alarmMaterialDataList) {
+        this.alarmList.addAll(alarmMaterialDataList);
+        if (this.alarmList.size() > ALARM_WRITE_COUNT) {
+           writeAlarm();
+        }
+    }
+
+    private synchronized void writeAlarm() {
+        try (FileWriter fileWriter = new FileWriter(new File(""),true)) {
+            for (AlarmMaterialData alarmMaterialData : this.alarmList) {
+                fileWriter.write(alarmMaterialData.toString());
+            }
+            this.alarmList.clear();
         } catch (IOException e) {
             e.printStackTrace();
         }
@@ -68,10 +99,10 @@ public class AssetReceiver implements Receiver {
 
     public enum AssetFlag {
 
-        NOT_ASSET(0, (json,detector) -> null),
+        NOT_ASSET(0, (json, detector) -> null),
         CLIENT_ASSET(1, AssetConfigs::detectorClient),
-        SERVER_ASSET(2,  AssetConfigs::detectorServer),
-        SERVER_AND_CLIENT_ASSET(3,  AssetConfigs::detectorClientAndServer);
+        SERVER_ASSET(2, AssetConfigs::detectorServer),
+        SERVER_AND_CLIENT_ASSET(3, AssetConfigs::detectorClientAndServer);
 
         private int flag;
 
