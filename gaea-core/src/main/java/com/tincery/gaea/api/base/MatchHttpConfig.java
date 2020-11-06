0e34be027d12c8853db6545b0daeae642b8afce8
gxz <514190950@qq.com>
2020-11-06 17:33:27 +0800

    更新alarm内容

--- gaea-core/src/main/java/com/tincery/gaea/api/base/MatchHttpConfig.java
+++ gaea-core/src/main/java/com/tincery/gaea/api/base/MatchHttpConfig.java
@@ -67,6 +67,10 @@ public class MatchHttpConfig extends SimpleBaseDO {
                 return require;
             }
             String value = content.substring(startIndex, endIndex);
+            // 如果value值是"" 直接返回 不打印在info上面
+            if(value.isEmpty()){
+                return true;
+            }
             JSONArray info = jsonObject.getJSONArray("information");
             if(CollectionUtils.isEmpty(info)){
                 info = new JSONArray();
