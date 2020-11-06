9d531e2acbb10bd624ab3e8cb8de510e784e0bab
w8782368 <395647962@qq.com>
2020-11-06 09:37:55 +0800

    修改isakmp监听问题

--- gaea-src-isakmp/src/main/java/com/tincery/gaea/source/isakmp/execute/MessageListener.java
+++ gaea-src-isakmp/src/main/java/com/tincery/gaea/source/isakmp/execute/MessageListener.java
@@ -1,11 +1,13 @@
 package com.tincery.gaea.source.isakmp.execute;
 
+import com.tincery.gaea.api.base.QueueNames;
 import com.tincery.gaea.core.base.component.Receiver;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jms.annotation.JmsListener;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
+import javax.jms.Queue;
 import javax.jms.TextMessage;
 
 @Component
@@ -14,7 +16,7 @@ public class MessageListener {
     @Autowired
     private Receiver receiver;
 
-    @JmsListener(destination = "src_impsession")
+    @JmsListener(destination = QueueNames.SRC_ISAKMP)
     public void receive(TextMessage textMessage) throws JMSException {
         System.out.println("接收到了内容");
         receiver.receive(textMessage);
