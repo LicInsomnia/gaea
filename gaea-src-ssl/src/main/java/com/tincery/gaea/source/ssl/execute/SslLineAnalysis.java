56f2d49745636763d7f8392e16f2562a2b06cb52
w8782368 <395647962@qq.com>
2020-11-09 15:05:28 +0800

    ssl模块修改  因为malformed的数据直接return 了  导致sslExtension为null  空指针  ；已修复

--- gaea-src-ssl/src/main/java/com/tincery/gaea/source/ssl/execute/SslLineAnalysis.java
+++ gaea-src-ssl/src/main/java/com/tincery/gaea/source/ssl/execute/SslLineAnalysis.java
@@ -43,6 +43,7 @@ public class SslLineAnalysis implements SrcLineAnalysis<SslData> {
         SslExtension sslExtension = new SslExtension();
         if (sslData.getDataType() == -1) {
             this.sslLineSupport.setMalformedPayload(elements[29], elements[30], sslData);
+            sslData.setSslExtension(sslExtension);
             return sslData;
         } else {
             if (elements[29].contains("malformed")) {
