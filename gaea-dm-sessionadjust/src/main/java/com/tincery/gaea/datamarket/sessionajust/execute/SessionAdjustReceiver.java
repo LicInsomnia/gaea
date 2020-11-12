09305b80ebce2bbc413f283a797fedcfebd5be8e
Insomnia <369528461@qq.com>
2020-11-12 11:51:31 +0800

    修改DW层抽象方法

--- gaea-dm-sessionadjust/src/main/java/com/tincery/gaea/datamarket/sessionajust/execute/SessionAdjustReceiver.java
+++ gaea-dm-sessionadjust/src/main/java/com/tincery/gaea/datamarket/sessionajust/execute/SessionAdjustReceiver.java
@@ -1,7 +1,9 @@
 package com.tincery.gaea.datamarket.sessionajust.execute;
 
 import com.tincery.gaea.core.base.component.Receiver;
+import com.tincery.gaea.core.base.component.support.DnsRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 
 import javax.jms.JMSException;
@@ -11,6 +13,9 @@ import java.io.File;
 @Slf4j
 @Component
 public class SessionAdjustReceiver implements Receiver {
+
+    @Autowired
+    private DnsRequest dnsRequest;
 
     @Override
     public void receive(TextMessage textMessage) throws JMSException {
