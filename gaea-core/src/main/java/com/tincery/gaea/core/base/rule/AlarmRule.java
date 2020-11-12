430622ee0e758fca86fee1c4cde04af27e7ebee6
w8782368 <395647962@qq.com>
2020-11-12 20:41:19 +0800

    AlarmRule  手动填入的pattern取消

--- gaea-core/src/main/java/com/tincery/gaea/core/base/rule/AlarmRule.java
+++ gaea-core/src/main/java/com/tincery/gaea/core/base/rule/AlarmRule.java
@@ -189,7 +189,7 @@ public class AlarmRule extends BaseSimpleRule {
                 }
                 AlarmMaterialData alarmMaterialData = new AlarmMaterialData(data, this.source, context, AlarmRule.this.ipSelector);
                 /*alarmRule 是 src告警*/
-                alarmMaterialData.setPattern(1);
+                /*alarmMaterialData.setPattern(1);*/
                 pushAlarm(alarmMaterialData, null);
             }
             return false;
