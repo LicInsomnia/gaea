c26d2d8f8480a0b95ab9b0b95e2be4c675feb897
Insomnia <369528461@qq.com>
2020-11-17 17:12:39 +0800

    修改DW层抽象方法

---
+++ gaea-src-espandah/src/main/java/com/tincery/gaea/source/espandah/execute/EspAndAhRecordSupport.java
@@ -0,0 +1,12 @@
+package com.tincery.gaea.source.espandah.execute;
+
+import com.tincery.gaea.api.base.EspAndAhRecord;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class EspAndAhRecordSupport {
+
+    private final Map<String, EspAndAhRecord> espAndAhRecordMap = new HashMap<>();
+
+}
