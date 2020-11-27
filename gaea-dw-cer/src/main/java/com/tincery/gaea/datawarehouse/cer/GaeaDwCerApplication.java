ffbdf32a5c8186384074e965b26f0d225ad919ae
LiuMing <412110252@qq.com>
2020-11-27 15:12:14 +0800

    证书分析模块加密强度检测（未完）

---
+++ gaea-dw-cer/src/main/java/com/tincery/gaea/datawarehouse/cer/GaeaDwCerApplication.java
@@ -0,0 +1,13 @@
+package com.tincery.gaea.datawarehouse.cer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+    public class GaeaDwCerApplication {
+
+        public static void main(String[] args) {
+            SpringApplication.run(GaeaDwCerApplication.class, args);
+        }
+
+}
