8650758ed8caa1cdbda3d6490a904e30ff2faa5d
Insomnia <369528461@qq.com>
2020-11-27 20:30:23 +0800

    修改资产拓展信息 新增告警统计

--- gaea-dm-alarmstatistic/src/main/java/com/tincery/gaea/datamarket/alarmstatistic/execute/AlarmStatisticReceiver.java
+++ gaea-dm-alarmstatistic/src/main/java/com/tincery/gaea/datamarket/alarmstatistic/execute/AlarmStatisticReceiver.java
@@ -12,6 +12,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
+import org.springframework.data.mongodb.core.query.Update;
 import org.springframework.stereotype.Service;
 
 import javax.jms.TextMessage;
@@ -71,7 +72,8 @@ public class AlarmStatisticReceiver extends AbstractDataMarketReceiver {
         log.info("检测到最新告警{}条", alarms.size());
         Map<String, List<MergeAble>> statisticMap = alarmStatistic(alarms);
         updateStatistic(statisticMap);
-        System.out.println(1);
+        Update update = new Update().set("handle", true);
+        this.alarmDao.update(query, update);
     }
 
     private Map<String, List<MergeAble>> alarmStatistic(List<Alarm> alarms) {
@@ -83,14 +85,6 @@ public class AlarmStatisticReceiver extends AbstractDataMarketReceiver {
         Map<String, MergeAble> mergedMap = new HashMap<>();
         collect.forEach((id, list) -> list.forEach(mergeAble -> mergedMap.merge(id, mergeAble, (k, v) -> (MergeAble) v.merge(mergeAble))));
         return mergedMap.values().stream().collect(Collectors.groupingBy((x) -> x.getId().split("\\.")[0]));
-
-//        for (Map.Entry<String, List<MergeAble>> entry : allStatistic.entrySet()) {
-//            String clazz = entry.getKey();
-//            List<MergeAble> mergeAbles = entry.getValue();
-//            SimpleBaseDaoImpl dao = (SimpleBaseDaoImpl) this.applicationContext.getBean(clazz);
-////            MergeSupport.rechecking(dao, mergeAbles);
-//        }
-//        log.info("{}合并插入{}条数据", alarmStatisticDao.getDbName(), collect.size());
     }
 
     private List<MergeAble<?>> createMergeableByAlarm(Alarm alarm) {
@@ -121,27 +115,105 @@ public class AlarmStatisticReceiver extends AbstractDataMarketReceiver {
     private void updateStatistic(Map<String, List<MergeAble>> statisticMap) {
         for (Map.Entry<String, List<MergeAble>> entry : statisticMap.entrySet()) {
             String key = entry.getKey();
+            int count = 0;
             switch (key) {
                 case "AlarmStatistic":
                     List<AlarmStatistic> alarmStatistics = new ArrayList<>();
                     for (MergeAble mergeAble : entry.getValue()) {
                         AlarmStatistic alarmStatistic = (AlarmStatistic) mergeAble;
                         alarmStatistics.add(alarmStatistic);
+                        count++;
                     }
                     MergeSupport.rechecking(this.alarmStatisticDao, alarmStatistics);
+                    log.info("{}合并插入{}条数据", alarmStatisticDao.getDbName(), count);
                     break;
                 case "AlarmCategoryStatistic":
                     List<AlarmCategoryStatistic> alarmCategoryStatistics = new ArrayList<>();
                     for (MergeAble mergeAble : entry.getValue()) {
                         AlarmCategoryStatistic alarmCategoryStatistic = (AlarmCategoryStatistic) mergeAble;
                         alarmCategoryStatistics.add(alarmCategoryStatistic);
+                        count++;
                     }
                     MergeSupport.rechecking(this.alarmCategoryStatisticDao, alarmCategoryStatistics);
+                    log.info("{}合并插入{}条数据", alarmCategoryStatisticDao.getDbName(), count);
+                    break;
+                case "AlarmCountryStatistic":
+                    List<AlarmCountryStatistic> alarmCountryStatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        AlarmCountryStatistic alarmCountryStatistic = (AlarmCountryStatistic) mergeAble;
+                        alarmCountryStatistics.add(alarmCountryStatistic);
+                        count++;
+                    }
+                    MergeSupport.rechecking(this.alarmCountryStatisticDao, alarmCountryStatistics);
+                    log.info("{}合并插入{}条数据", alarmCountryStatisticDao.getDbName(), count);
+                    break;
+                case "AlarmCountStatistic":
+                    List<AlarmCountStatistic> alarmCountStatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        AlarmCountStatistic alarmCountStatistic = (AlarmCountStatistic) mergeAble;
+                        alarmCountStatistics.add(alarmCountStatistic);
+                    }
+                    MergeSupport.rechecking(this.alarmCountStatisticDao, alarmCountStatistics);
+                    log.info("{}合并插入{}条数据", alarmCountStatisticDao.getDbName(), count);
+                    break;
+                case "AlarmLevelStatistic":
+                    List<AlarmLevelStatistic> alarmLevelStatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        AlarmLevelStatistic alarmLevelStatistic = (AlarmLevelStatistic) mergeAble;
+                        alarmLevelStatistics.add(alarmLevelStatistic);
+                    }
+                    MergeSupport.rechecking(this.alarmLevelStatisticDao, alarmLevelStatistics);
+                    log.info("{}合并插入{}条数据", alarmLevelStatisticDao.getDbName(), count);
+                    break;
+                case "AlarmSslStatistic":
+                    List<AlarmSslStatistic> alarmSslStatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        AlarmSslStatistic alarmSslStatistic = (AlarmSslStatistic) mergeAble;
+                        alarmSslStatistics.add(alarmSslStatistic);
+                    }
+                    MergeSupport.rechecking(this.alarmSslStatisticDao, alarmSslStatistics);
+                    log.info("{}合并插入{}条数据", alarmSslStatisticDao.getDbName(), count);
+
+                    break;
+                case "AlarmUnitStatistic":
+                    List<AlarmUnitStatistic> alarmUnitStatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        AlarmUnitStatistic alarmUnitStatistic = (AlarmUnitStatistic) mergeAble;
+                        alarmUnitStatistics.add(alarmUnitStatistic);
+                    }
+                    MergeSupport.rechecking(this.alarmUnitStatisticDao, alarmUnitStatistics);
+                    log.info("{}合并插入{}条数据", alarmUnitStatisticDao.getDbName(), count);
+                    break;
+                case "AlarmUserStatistic":
+                    List<AlarmUserStatistic> alarmUserStatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        AlarmUserStatistic alarmUserStatistic = (AlarmUserStatistic) mergeAble;
+                        alarmUserStatistics.add(alarmUserStatistic);
+                    }
+                    MergeSupport.rechecking(this.alarmUserStatisticDao, alarmUserStatistics);
+                    log.info("{}合并插入{}条数据", alarmUserStatisticDao.getDbName(), count);
+                    break;
+                case "ImpAlarmCategoryStatistic":
+                    List<ImpAlarmCategoryStatistic> impAlarmCategoryStatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        ImpAlarmCategoryStatistic impAlarmCategoryStatistic = (ImpAlarmCategoryStatistic) mergeAble;
+                        impAlarmCategoryStatistics.add(impAlarmCategoryStatistic);
+                    }
+                    MergeSupport.rechecking(this.impAlarmCategoryStatisticDao, impAlarmCategoryStatistics);
+                    log.info("{}合并插入{}条数据", impAlarmCategoryStatisticDao.getDbName(), count);
+                    break;
+                case "ImpAlarmTargetStatistic":
+                    List<ImpAlarmTargetStatistic> impAlarmTargettatistics = new ArrayList<>();
+                    for (MergeAble mergeAble : entry.getValue()) {
+                        ImpAlarmTargetStatistic impAlarmTargetStatistic = (ImpAlarmTargetStatistic) mergeAble;
+                        impAlarmTargettatistics.add(impAlarmTargetStatistic);
+                    }
+                    MergeSupport.rechecking(this.impAlarmTargetStatisticDao, impAlarmTargettatistics);
+                    log.info("{}合并插入{}条数据", impAlarmTargetStatisticDao.getDbName(), count);
                     break;
                 default:
                     break;
             }
-            System.out.println(1);
         }
     }
 
