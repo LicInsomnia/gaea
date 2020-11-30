96583cc18f815eb3a32712a37ab145103faad184
LiuMing <412110252@qq.com>
2020-11-30 21:09:10 +0800

    证书分析模块加密强度检测（增加低指数攻击检测；缺少密钥交换检测）

--- gaea-dw-cer/src/main/java/com/tincery/gaea/datawarehouse/cer/execute/CerEncryptionStrengthModule.java
+++ gaea-dw-cer/src/main/java/com/tincery/gaea/datawarehouse/cer/execute/CerEncryptionStrengthModule.java
@@ -5,6 +5,8 @@ import com.tincery.gaea.core.base.component.config.NodeInfo;
 import com.tincery.gaea.core.base.tool.moduleframe.BaseModule;
 import com.tincery.gaea.core.base.tool.moduleframe.BaseModuleInterface;
 import com.tincery.gaea.core.base.tool.moduleframe.DataQueue;
+
+import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
 
@@ -38,25 +40,33 @@ public class CerEncryptionStrengthModule extends BaseModule implements BaseModul
                     String eStr = cer.getRsaE();
                     if(nStr.length() > 0 && eStr.length() > 0) {
                         try {
-                            if(primalityDetect(nStr, eStr)) {
-                                cer.setPrimalityDetectResult(1);
-                            } else {
-                                cer.setPrimalityDetectResult(0);
+                            List<Integer> resultList = new ArrayList<>();
+                            cer.setPrimalityDetectResult(primalityDetect(nStr, eStr) ? 1 : 0);
+                            resultList.add(cer.getPrimalityDetectResult());
+                            cer.setSmallFactorDetectResult(smallFactorDetect(nStr, eStr) ? 1 : 0);
+                            resultList.add(cer.getSmallFactorDetectResult());
+                            cer.setRandomPrimeFactorDetectResult(randomPrimeFactorDetect(nStr, eStr) ? 1 : 0);
+                            resultList.add(cer.getRandomPrimeFactorDetectResult());
+                            cer.setRsaFixedPointNumberDetectResult(rsaFixedPointNumberDetect(nStr, eStr) ? 1 : 0);
+                            resultList.add(cer.getRsaFixedPointNumberDetectResult());
+                            cer.setLowIndexAttackDetectResult(lowIndexAttackDetect(nStr, eStr) ? 1 : 0);
+                            resultList.add(cer.getLowIndexAttackDetectResult());
+                            boolean negFlag = false;
+                            boolean zeroFlag = false;
+                            for(Integer result : resultList) {
+                                if(result.equals(0)) {
+                                    zeroFlag = true;
+                                }
+                                if(result.equals(-1)) {
+                                    negFlag = true;
+                                }
                             }
-                            if(smallFactorDetect(nStr, eStr)) {
-                                cer.setSmallFactorDetectResult(1);
+                            if(negFlag) {
+                                cer.setRsaSecurityStatus(-1);
+                            } else if(zeroFlag) {
+                                cer.setRsaSecurityStatus(0);
                             } else {
-                                cer.setSmallFactorDetectResult(0);
-                            }
-                            if(randomPrimeFactorDetect(nStr, eStr)) {
-                                cer.setRandomPrimeFactorDetectResult(1);
-                            } else {
-                                cer.setRandomPrimeFactorDetectResult(0);
-                            }
-                            if(rsaFixedPointNumberDetect(nStr, eStr)) {
-                                cer.setRsaFixedPointNumberDetectResult(1);
-                            } else {
-                                cer.setRsaFixedPointNumberDetectResult(0);
+                                cer.setRsaSecurityStatus(1);
                             }
                         } catch (Exception e) {
                             e.printStackTrace();
