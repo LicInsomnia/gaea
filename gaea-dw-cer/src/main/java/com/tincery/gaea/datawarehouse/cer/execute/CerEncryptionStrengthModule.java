89b61cae4fc039e45c5a7c1e85f5a892e5610982
LiuMing <412110252@qq.com>
2020-12-01 21:53:32 +0800

    证书分析模块加密强度检测（增加密钥交换检测）

--- gaea-dw-cer/src/main/java/com/tincery/gaea/datawarehouse/cer/execute/CerEncryptionStrengthModule.java
+++ gaea-dw-cer/src/main/java/com/tincery/gaea/datawarehouse/cer/execute/CerEncryptionStrengthModule.java
@@ -36,44 +36,48 @@ public class CerEncryptionStrengthModule extends BaseModule implements BaseModul
             while (true) {
                 CerData cer = (CerData) queueInput.poll(1, TimeUnit.SECONDS);
                 if (cer != null) {
-                    String nStr = cer.getRsaN();
-                    String eStr = cer.getRsaE();
-                    if(nStr.length() > 0 && eStr.length() > 0) {
-                        try {
-                            List<Integer> resultList = new ArrayList<>();
-                            cer.setPrimalityDetectResult(primalityDetect(nStr, eStr) ? 1 : 0);
-                            resultList.add(cer.getPrimalityDetectResult());
-                            cer.setSmallFactorDetectResult(smallFactorDetect(nStr, eStr) ? 1 : 0);
-                            resultList.add(cer.getSmallFactorDetectResult());
-                            cer.setRandomPrimeFactorDetectResult(randomPrimeFactorDetect(nStr, eStr) ? 1 : 0);
-                            resultList.add(cer.getRandomPrimeFactorDetectResult());
-                            cer.setRsaFixedPointNumberDetectResult(rsaFixedPointNumberDetect(nStr, eStr) ? 1 : 0);
-                            resultList.add(cer.getRsaFixedPointNumberDetectResult());
-                            cer.setLowIndexAttackDetectResult(lowIndexAttackDetect(nStr, eStr) ? 1 : 0);
-                            resultList.add(cer.getLowIndexAttackDetectResult());
-                            String datPath = NodeInfo.getConfig() + "/pubkeyCheck/";
-                            cer.setKeyExchangeLeakDetectResult(keyExchangeLeakDetect(nStr, eStr, datPath) ? 1 : 0);
-
-                            boolean negFlag = false;
-                            boolean zeroFlag = false;
-                            for(Integer result : resultList) {
-                                if(result.equals(0)) {
-                                    zeroFlag = true;
+                    if(cer.getSubjectPublicKeyInfoAlgorithmOid().equals("1.2.840.113549.1.1.1")) {
+                        String nStr = cer.getRsaN();
+                        String eStr = cer.getRsaE();
+                        if(nStr.length() > 0 && eStr.length() > 0) {
+                            try {
+                                List<Integer> resultList = new ArrayList<>();
+                                cer.setPrimalityDetectResult(primalityDetect(nStr, eStr) ? 1 : 0);
+                                resultList.add(cer.getPrimalityDetectResult());
+                                cer.setSmallFactorDetectResult(smallFactorDetect(nStr, eStr) ? 1 : 0);
+                                resultList.add(cer.getSmallFactorDetectResult());
+                                cer.setRandomPrimeFactorDetectResult(randomPrimeFactorDetect(nStr, eStr) ? 1 : 0);
+                                resultList.add(cer.getRandomPrimeFactorDetectResult());
+                                cer.setRsaFixedPointNumberDetectResult(rsaFixedPointNumberDetect(nStr, eStr) ? 1 : 0);
+                                resultList.add(cer.getRsaFixedPointNumberDetectResult());
+                                cer.setLowIndexAttackDetectResult(lowIndexAttackDetect(nStr, eStr) ? 1 : 0);
+                                resultList.add(cer.getLowIndexAttackDetectResult());
+                                boolean negFlag = false;
+                                boolean zeroFlag = false;
+                                for(Integer result : resultList) {
+                                    if(result.equals(0)) {
+                                        zeroFlag = true;
+                                    }
+                                    if(result.equals(-1)) {
+                                        negFlag = true;
+                                    }
                                 }
-                                if(result.equals(-1)) {
-                                    negFlag = true;
+                                if(negFlag) {
+                                    cer.setRsaSecurityStatus(-1);
+                                } else if(zeroFlag) {
+                                    cer.setRsaSecurityStatus(0);
+                                } else {
+                                    cer.setRsaSecurityStatus(1);
                                 }
+                            } catch (Exception e) {
+                                e.printStackTrace();
                             }
-                            if(negFlag) {
-                                cer.setRsaSecurityStatus(-1);
-                            } else if(zeroFlag) {
-                                cer.setRsaSecurityStatus(0);
-                            } else {
-                                cer.setRsaSecurityStatus(1);
-                            }
-                        } catch (Exception e) {
-                            e.printStackTrace();
                         }
+                    } else if(cer.getSubjectPublicKeyInfoAlgorithmOid().equals("1.2.840.10045.2.1")) {
+                        String xStr = cer.getEccX();
+                        String yStr = cer.getEccY();
+                        String datPath = NodeInfo.getConfig() + "/pubkeyCheck/";
+                        cer.setKeyExchangeLeakDetectResult(keyExchangeLeakDetect(xStr, yStr, datPath) ? 1 : 0);
                     }
                     queueOutput.put(cer);
                 }
